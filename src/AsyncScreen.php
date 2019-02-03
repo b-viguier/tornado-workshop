f863698224f5dd93fe0fef2842062c7ad50fa76d
Benoit Viguier <benoit.viguier@m6.fr>
2019-02-03 11:51:17 +0100

    Add AsyncScreen

---
+++ src/AsyncScreen.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Workshop\Async;
+
+use M6Web\Tornado\EventLoop;
+use M6Web\Tornado\Promise;
+
+class AsyncScreen
+{
+
+    public function __construct(EventLoop $eventLoop)
+    {
+        $this->eventLoop = $eventLoop;
+    }
+
+    public function display(): Promise
+    {
+        $this->startTime = microtime(true);
+
+        return $this->eventLoop->async($this->loop());
+    }
+
+    /** @var EventLoop */
+    private $eventLoop;
+
+    /** @var int */
+    private $startTime = 0;
+
+    private function loop(): \Generator
+    {
+        while (true) {
+            yield $this->eventLoop->idle();
+
+            $elapsedTime = round(microtime(true) - $this->startTime, 1);
+
+            echo "Elapsed: $elapsedTime\t\r";
+        }
+    }
+}
